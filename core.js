a3a114a8d8375f702305f9b49a345faff63b5eaf
Qwerty-pixl <gulanda727@gmail.com>
2023-12-28 23:10:14 +0600

    i am snova forgotting about github

--- core.js
+++ core.js
@@ -1,40 +1,29 @@
-const burger = document.querySelector("#burger");
-const menu_burger = document.querySelector("#menu_burger");
-const body = document.body;
+(function(){
 
-// Клонируем меню, чтобы задать свои стили для мобильной версии
-const menu = document.querySelector("#menu").cloneNode(1);
-
-// При клике на иконку hamb вызываем ф-ию hambHandler
-burger.addEventListener("click", burgerHandler);
-
-// Выполняем действия при клике ..
-function burgerHandler(e) {
-  e.preventDefault();
-  // Переключаем стили элементов при клике
-  menu_burger.classList.toggle("open");
-  burger.classList.toggle("active");
-  body.classList.toggle("noscroll");
-  renderPopup();
-}
-
-// Здесь мы рендерим элементы в наш попап
-function renderPopup() {
-  menu_burger.appendChild(menu);
-}
-
-// Код для закрытия меню при нажатии на ссылку
-const links = Array.from(menu.children);
-
-// Для каждого элемента меню при клике вызываем ф-ию
-links.forEach((link) => {
-  link.addEventListener("click", closeOnClick);
-});
-
-// Закрытие попапа при клике на меню
-function closeOnClick() {
-  menu_burger.classList.remove("open");
-  burger.classList.remove("active");
-  body.classList.remove("noscroll");
-}
-
+  let counter = document.querySelectorAll('.counter');
+  let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
+  window.addEventListener('scroll', function(){  
+    if( limit == counter.length ){ return; }
+    
+    for(let i = 0; i < counter.length; i++){
+      let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
+      let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
+      if( pos < win && counter[i].dataset.stop === "0" ){
+        counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
+        let x = 0;
+        limit++; // Счетчик будет запущен, увеличиваем переменную на 1
+        let int = setInterval(function(){
+          // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
+          x = x + Math.ceil( counter[i].dataset.to / 50 ); 
+          counter[i].innerText = x;
+          if( x > counter[i].dataset.to ){
+            //Как только досчитали - стираем интервал.
+            counter[i].innerText = counter[i].dataset.to;
+            clearInterval(int);
+          }
+        }, 100);
+      }
+    }
+  });
+  
+  })();
