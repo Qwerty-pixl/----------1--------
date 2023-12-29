7b269eabf7b39df84526c13405041dd455f43054
Qwerty-pixl <gulanda727@gmail.com>
2023-12-29 08:36:58 +0600

    added slider

--- core.js
+++ core.js
@@ -27,3 +27,16 @@
   });
   
   })();
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
